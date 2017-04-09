d0f069d9ab7c0503cd36b37d950576fd4611b2b7
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-04-09 15:57:06 +0900

    add solution

---
+++ TryCSharp.Tools.Cui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TryCSharp.Tools.Cui
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
