7e94d57f0347179f83a49600def4ed5517f96210
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-06-21 17:10:02 +0900

    ported from Sazare.

---
+++ TryCSharp.Samples/Linq/LinqSamples75.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using TryCSharp.Common;
+
+namespace TryCSharp.Samples.Linq
+{
+    /// <summary>
+    ///     LINQ to XMLのサンプルです.
+    /// </summary>
+    /// <remarks>
+    ///     ナビゲーション(Parentプロパティ)のサンプルです.
+    /// </remarks>
+    [Sample]
+    public class LinqSamples75 : IExecutable
+    {
+        public void Execute()
+        {
+            //
+            // Parent
+            //   文字通り、現在の要素の親要素を取得する.
+            //   親要素が存在しない場合、nullとなる.
+            //
+            var root = BuildSampleXml();
+            var elem = root.Elements("Child").First();
+
+            Output.WriteLine("root.Parent = {0}", root.Parent?.ToString() ?? "null");
+            Output.WriteLine("elem.Parent = {0}", elem.Parent);
+
+            var newElem = new XElement("GrandChild", "value5");
+            Output.WriteLine("newElem.Parent = {0}", newElem.Parent?.ToString() ?? "null");
+
+            root.Elements("Child").Last().Add(newElem);
+            Output.WriteLine("newElem.Parent = {0}", newElem.Parent);
+        }
+
+        private XElement BuildSampleXml()
+        {
+            var root = new XElement("Root",
+                new XElement("Child", "value1"),
+                new XElement("Child", "value2"),
+                new XElement("Child", "value3"),
+                new XElement("Child", "value4")
+            );
+
+            return root;
+        }
+    }
+}
