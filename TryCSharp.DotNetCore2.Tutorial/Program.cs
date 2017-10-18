e283eddf4e5f7afea8aa94464509cb31fed8b0b3
devlights <gsf.zero1@devlights.onmicrosoft.com>
2017-10-18 13:39:58 +0900

    Add TryCSharp.DotNetCore2.Tutorial project

---
+++ TryCSharp.DotNetCore2.Tutorial/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryCSharp.DotNetCore2.Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
