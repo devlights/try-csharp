2e86bd44dd8dcb71c305b96ac51bbfa0d6fe6867
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-03-09 04:44:46 +0000

    Fix warnings

--- TryCSharp.Tools.Cui/CuiOutputManager.cs
+++ TryCSharp.Tools.Cui/CuiOutputManager.cs
@@ -13,13 +13,13 @@ namespace TryCSharp.Tools.Cui
         /// 指定されたデータを出力します。（改行付与無し）
         /// </summary>
         /// <param name="data">データ</param>
-        public void Write(object data) => Console.Write(data);
+        public void Write(object? data) => Console.Write(data);
 
         /// <summary>
         /// 指定されたデータを出力します。（改行付与有り）
         /// </summary>
         /// <param name="data">データ</param>
-        public void WriteLine(object data) => Console.WriteLine(data);
+        public void WriteLine(object? data) => Console.WriteLine(data);
 
         /// <summary>
         /// 出力ストリーム
